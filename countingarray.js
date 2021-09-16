e412c701c23aa5b4a48d57881e4ea06dbe728205
carolynegreen <carolynemarie94@gmail.com>
2021-09-16 10:47:07 +0200

    finished

--- countingarray.js
+++ countingarray.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const array = [];
+let counting = 0;
+countArray();
+
+function countArray() {
+  counting++;
+  array.unshift(counting);
+  console.log(array.slice(0, 9));
+  setTimeout(countArray, 600);
+}
